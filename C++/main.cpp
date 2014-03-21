c254ab88e216f710c46710a3431d3c4018cc5367
JP <jp41011@gmail.com>
2014-03-20 23:20:27 -0700

    Created folders
    Created folders that will be filled out later

---
+++ C++/main.cpp
@@ -0,0 +1,11 @@
+// Juan Pedraza
+/* Will implement several different sorting algorithms
+using list and array data structures. Will also compare runtime
+of the different sorting algorithms
+*/
+
+int main()
+{
+
+	return 0;
+}
