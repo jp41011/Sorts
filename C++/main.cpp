df032dbaf845314e2add128721385d8aff0b91d9
Juan <jp41011@gmail.com>
2014-03-27 12:16:16 -0700

    added a little bit to main.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,23 @@
 // Juan P - C++
-/* Will implement several different sorting algorithms
-using list and array data structures. Will also compare runtime
+/* Will implement several different sorting algorithms.
+Will also compare runtime
 of the different sorting algorithms
 */
+#include <stdlib.h> // rand - random numbers
+#include <vector>
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int elements;
+	vector<int> 
+	cout << "Comparing Sorting Algorithms" << endl;
+	cout << "Number(pos) of random elements: ";
+	cin >> elements;
+	//generate random numbers
+	for(unsigned short i=0; i<elements; i++)
+
 
 	return 0;
 }
